fc6e813f906086a786075fa2f144eb060860c250
karlit00 <kgabutero.18@gmail.com>
2026-04-23 14:38:02 +0800

    nomobile

--- mobile/api/auth.js
+++
@@ -1,9 +0,0 @@
-import API from "./client";
-
-export const registerUser = (data) => {
-  return API.post("/auth/signup", data).then(res => res.data);
-};
-
-export const loginUser = (data) => {
-  return API.post("/auth/login", data).then(res => res.data);
-};
